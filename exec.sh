392b018780023db286110b9223052f4af6f84aaa
nia <nia@nia-s-beast.(none)>
2013-01-19 12:40:17 +0100

    Modification du repertoire de sortie

--- exec.sh
+++ exec.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+prefix="OUTPUT"
+
 while getopts f:v: option
   do
     case $option in
@@ -13,14 +15,14 @@ for f in $(find $folder -iname "*.txt");
   do
     a=${f##*/}
     a=${a%.txt}
-    mkdir $a
-    ./segmenteur.pl $f $a/$a.seg
-    ./filtrage.pl $a/$a.seg $a/$a.fil
+    mkdir $prefix/$a
+    ./segmenteur.pl $f $prefix/$a/$a.seg
+    ./filtrage.pl $prefix/$a/$a.seg $prefix/$a/$a.fil
     echo "Quelle est la langue du document $a.txt ? (eng, esp ou fra)"
     read langue
-    ./fonctionnel.pl $a/$a.fil $a/$a.fon $langue
+    ./fonctionnel.pl $prefix/$a/$a.fil $prefix/$a/$a.fon $langue
     echo "Quel stemmer voulez vous utiliser ? (1 pour l'ultra-stemming / 2 pour Porter)"
     read stemmer
-    ./normalise.pl $a/$a.fon $a/$a.lemm $langue $stemmer
+    ./normalise.pl $prefix/$a/$a.fon $prefix/$a/$a.lemm $langue $stemmer
     #./PACKAGE_ROUGE_FRESA/fresa.sh  $f
   done
